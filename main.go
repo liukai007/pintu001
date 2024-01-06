4f924e3d576494e224deb6174154a6d9ba62a3d1
刘凯 <850770722@qq.com>
2024-01-06 23:00:59 +0800

    文件拖拽

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"bytes"
 	_ "embed"
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
-	"image"
-	"image/draw"
 )
 
 //go:embed swk.jpg
@@ -25,59 +24,27 @@ https://www.bilibili.com/video/BV1zD4y167UL
 func main() {
 	a := app.NewWithID("pintu")
 	win := a.NewWindow("embed image browser")
-	img := canvas.NewImageFromImage(nil)
-	n := 3
-	right := container.NewGridWithColumns(n)
-	pieces := make([]fyne.CanvasObject, 9)
-	btn1 := widget.NewButton("Split", func() {
-		if img.Image == nil {
-			return
+	slider := widget.NewSlider(0, 1)
+	slider.Resize(fyne.NewSize(111, 11))
+	msgEntry := widget.NewMultiLineEntry()
+	msgEntry1 := widget.NewMultiLineEntry()
+	slider.Step = 0.01
+	slider.OnChanged = func(f float64) {
+		fmt.Println(f)
+	}
+	win.SetOnDropped(func(p fyne.Position, u []fyne.URI) {
+		paths := ""
+		for _, v := range u {
+			paths = paths + v.Path() + "\n"
 		}
-		rgb1 := ImageToRGBA(img.Image)
-		w := rgb1.Rect.Dx() / n
-		h := rgb1.Rect.Dy() / n
-		n0 := 0
-		for i := 0; i < n; i++ {
-			for j := 0; j < n; j++ {
-				v := canvas.NewImageFromImage(rgb1.SubImage(
-					image.Rect(w*j, h*i, w*(j+1), h*(i+1))))
-				pieces[n0] = v
-				n0++
-			}
-		}
-		right.Objects = pieces
-		right.Refresh()
+		fmt.Println(paths)
+		msgEntry.SetText(paths)
 	})
-	btnSwk := widget.NewButton("swk", func() {
-		SetPic(img, swk)
-		btn1.OnTapped()
-	})
-	btnZbj := widget.NewButton("zbj", func() {
-		SetPic(img, zbj)
-		btn1.OnTapped()
-	})
-
-	top := container.NewHBox(btnSwk, btnZbj, btn1)
-	cc := container.NewHSplit(img, right)
-	win.SetContent(container.NewBorder(top, nil, nil, nil, cc))
+	//title := canvas.NewText("往窗口上拖拉文件演示，滑块控件点击演示", theme.PrimaryColor())
+	title := canvas.NewText("desktop", theme.PrimaryColor())
+	top := container.NewVBox(msgEntry, msgEntry1)
+	win.SetContent(container.NewBorder(title, slider, nil, nil, top))
 	win.SetMaster()
 	win.Resize(fyne.NewSize(800, 700))
 	win.ShowAndRun()
-}
-
-func SetPic(img *canvas.Image, picData []byte) (err error) {
-	buf := bytes.NewReader(picData)
-	img.Image, _, err = image.Decode(buf)
-	if err != nil {
-		return err
-	}
-	img.Refresh()
-	return nil
-}
-
-func ImageToRGBA(src image.Image) *image.RGBA {
-	bounds := src.Bounds()
-	dst := image.NewRGBA(bounds)
-	draw.Draw(dst, bounds, src, bounds.Min, draw.Src)
-	return dst
 }
