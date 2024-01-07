bb8feefb9171f702430e79123ca46bacfc584529
刘凯 <850770722@qq.com>
2024-01-07 21:23:54 +0800

    ProgressBar 控件

---
+++ aa1_appTabs.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	//"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("TabContainer Widget")
+
+	tabs2 := container.NewAppTabs(
+		container.NewTabItem("Tab 1", widget.NewLabel("Hello")),
+		container.NewTabItem("Tab 2", widget.NewLabel("World!")),
+	)
+	tabs3 := container.NewAppTabs(
+		container.NewTabItem("Tab 3", widget.NewLabel("Hello3")),
+		container.NewTabItem("Tab 4", widget.NewLabel("World!3")),
+	)
+	tabs := container.NewAppTabs(
+		container.NewTabItem("Tab 1", tabs2),
+		container.NewTabItem("Tab 2", tabs3),
+	)
+
+	//tabs.Append(container.NewTabItemWithIcon("Home", theme.HomeIcon(), widget.NewLabel("Home tab")))
+
+	tabs.SetTabLocation(container.TabLocationLeading)
+
+	myWindow.SetContent(tabs)
+	myWindow.Resize(fyne.NewSize(800, 700))
+	myWindow.ShowAndRun()
+}
